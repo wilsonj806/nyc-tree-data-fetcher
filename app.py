ef32c6ac4d273f74249c794316b0ae7624ee80c9
Wilson <wilsonj806@gmail.com>
2020-03-11 16:05:48 -0400

    Add basic GET and data read from .env
    Update app.py so it loads data from .env and so it fetches data from our API

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
+# Std lib import
+import os
+
+# Library Imports
+import requests
 from flask import Flask
+from dotenv import load_dotenv
+
+# App Modules
+
+## Load ENV
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_there():
-  return 'Hello there!'
+def initial_ping():
+  return """NYC Tree Data Fetcher, Version: 0.0.0"""
+
+@app.route('/fetch-all')
+def fetch_all():
+  headers = { 'X-App-Token': os.getenv("TOKEN_SECRET") }
+  req = requests.get("https://data.cityofnewyork.us/resource/uvpi-gqnh.json", headers = headers)
+
+  json = req.json()
+  return {
+    '_data_length': len(json),
+    'data': json
+  }

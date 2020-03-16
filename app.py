27568079d991f151596deed3aa54b485a3637224
Wilson <wilsonj806@gmail.com>
2020-03-15 22:31:41 -0400

    Add route that returns a count of the tree species from data
    Updates to app.py
    - commented out dead code from app.py
    - update route names to better represent REST API standard
    - add a new route that returns quantities of each tree species
    
    Update the processor package with a new method to compute the quantities of different tree species

--- app.py
+++ app.py
@@ -23,19 +23,26 @@ fetcher = Fetch()
 def initial_ping():
   return """NYC Tree Data Fetcher; Version: 0.0.0"""
 
-@app.route('/fetch-all')
+@app.route('/data')
 def fetch_all():
-  # Check to make sure we're not in docker compose
   json = fetcher.check_cache()
-  print('fetched and returning')
   return {
     '_data_length': len(json),
     'data': json
   }
 
-@app.route('/count-per')
+@app.route('/data/count')
 def count_per_boro():
   json = fetcher.check_cache()
-  data = Processor.return_per_boro(data = json)
-  print('fetched and returning')
+  data = Processor.count_per_boro(data = json)
   return { 'data': data }
+
+@app.route('/data/species')
+def count_per_species():
+  json = fetcher.check_cache()
+  data = Processor.count_species(data = json)
+  return { 'data': data }
+
+# @app.route('/data/<boro>/species')
+# def quant_boro_species(boro):
+#   return 'n/a'
--- processor/processor.py
+++ processor/processor.py
@@ -9,10 +9,43 @@ def reduce_by_boro(acc, inputdict):
     acc[dictname] += 1
   return acc
 
+def reduce_by_species(acc, inputdict):
+  # IF the current entry is a Stump
+  if inputdict['status'] == 'Stump' and not 'stump' in acc:
+    newentry = { 'stump': 1 }
+    acc.update(newentry)
+    return acc
+  elif inputdict['status'] == 'Stump':
+    acc['stump'] += 1
+    return acc
+
+  # IF the current entry is Dead
+  if inputdict['status'] == 'Dead' and not 'dead' in acc:
+    newentry = { 'dead': 1 }
+    acc.update(newentry)
+    return acc
+  elif inputdict['status'] == 'Dead':
+    acc['dead'] += 1
+    return acc
+
+  # ELSE check if the tree exists
+  dictname = inputdict['spc_common']
+  if not dictname in acc:
+    newentry = { dictname: 1 }
+    acc.update(newentry)
+  else:
+    acc[dictname] += 1
+  return acc
+
 class Processor:
-  def return_per_boro(data):
+  def count_per_boro(data):
     if not data or not isinstance(data, list):
-      raise TypeError('You used it wrong')
+      raise TypeError('Expecting data to be a List')
     return reduce(reduce_by_boro, data, {})
 
+  def count_species(data):
+    print('dookie')
+    if not isinstance(data, list):
+      raise TypeError('Expecting boro to be a string')
+    return reduce(reduce_by_species, data, {})
 
